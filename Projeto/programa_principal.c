dadda1f37a1632a44526a1c5dd1c9b4f4637aaf2
arthur <103393571+THUR165@users.noreply.github.com>
2024-09-20 17:11:03 -0300

    adicionado tela_sobre e adicionado relatorio na tela principal

--- Projeto/programa_principal.c
+++ Projeto/programa_principal.c
@@ -2,30 +2,49 @@
 
 //funções principais
 void tela_principal(void);
+void tela_sobre(void);
 
 //programa principal
 int main(void){
     tela_principal();
+    tela_sobre();
 }
 
 
 void tela_principal(void){
     printf("\n");
-    printf("═════════════════════════════════════════════════════\n");
-    printf("═══                SIG-Building                   ═══\n");
-    printf("═════════════════════════════════════════════════════\n");
-    printf("═══ Sistema de gestão de material de construção   ═══\n");
-    printf("═════════════════════════════════════════════════════\n");
-    printf("═══    1-venda                                    ═══\n");
-    printf("═══    2-produto                                  ═══\n");
-    printf("═══    3-cliente                                  ═══\n");
-    printf("═══    4-funcionarios                             ═══\n");
-    printf("═══    5-sobre o programa                         ═══\n");
-    printf("═══    6-informaçoes dos desenvolvedores          ═══\n");
-    printf("═════════════════════════════════════════════════════\n");
-    printf("═════════════════════════════════════════════════════\n");
-    printf("═════════════════════════════════════════════════════\n");
-    printf("═════════════════════════════════════════════════════\n");
-
+    printf("═══════════════════════════════════════════════════════════\n");
+    printf("═══                SIG-Building                      ══════\n");
+    printf("═══════════════════════════════════════════════════════════\n");
+    printf("═══ Sistema de gestão de material de construção      ══════\n");
+    printf("═══════════════════════════════════════════════════════════\n");
+    printf("═══    1-venda                                        ═════\n");
+    printf("═══    2-produto                                      ═════\n");
+    printf("═══    3-cliente                                      ═════\n");
+    printf("═══    4-funcionarios                                 ═════\n");
+    printf("═══    5-Relatorio                                    ═════\n");
+    printf("═══    6-sobre o programa                             ═════\n");
+    printf("═══    7-informaçoes dos desenvolvedores              ═════\n");
+    printf("═══                                                   ═════\n");
+    printf("═══                                                   ═════\n");
+    printf("═══════════════════════════════════════════════════════════\n");
+    
 }
 
+void tela_sobre(void){
+    printf("\n");
+    printf("═══════════════════════════════════════════════════════════\n");
+    printf("═══   Universidade Federal Rio Grande do Norte        ═════\n");
+    printf("═══   Diciplina Programação  codigo:DCT1106           ═════\n");
+    printf("═══               Programa de Gestão                  ═════\n");
+    printf("═══   Para uma Loja de Material de Construção         ═════\n");
+    printf("═══════════════════════════════════════════════════════════\n");
+    printf("═══   O Projeto tem o objetivo Criar um programa      ═════\n");
+    printf("═══   de Gestão para uma Loja de Materais             ═════\n");
+    printf("═══   de Construção,tem como objetivo ajudar na       ═════\n");
+    printf("═══   administração da loja,sendo possivel administar ═════\n");
+    printf("═══   produtos,vendas,clientes,funcionarios e tambem  ═════\n");
+    printf("═══   relatorios                                      ═════\n");
+    printf("═══════════════════════════════════════════════════════════\n");
+
+}
