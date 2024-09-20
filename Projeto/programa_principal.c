ffb9410e1a8a60bdca2483b2971906289b811a34
arthur <103393571+THUR165@users.noreply.github.com>
2024-09-20 09:09:17 -0300

    adicionado tela principal tambem correção de erros ortograficos

---
+++ Projeto/programa_principal.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+//funções principais
+void tela_principal(void);
+
+//programa principal
+int main(void){
+    tela_principal();
+}
+
+
+void tela_principal(void){
+    printf("\n");
+    printf("═════════════════════════════════════════════════════\n");
+    printf("═══                SIG-Building                   ═══\n");
+    printf("═════════════════════════════════════════════════════\n");
+    printf("═══ Sistema de gestão de material de construção   ═══\n");
+    printf("═════════════════════════════════════════════════════\n");
+    printf("═══    1-venda                                    ═══\n");
+    printf("═══    2-produto                                  ═══\n");
+    printf("═══    3-cliente                                  ═══\n");
+    printf("═══    4-funcionarios                             ═══\n");
+    printf("═══    5-sobre o programa                         ═══\n");
+    printf("═══    6-informaçoes dos desenvolvedores          ═══\n");
+    printf("═════════════════════════════════════════════════════\n");
+    printf("═════════════════════════════════════════════════════\n");
+    printf("═════════════════════════════════════════════════════\n");
+    printf("═════════════════════════════════════════════════════\n");
+
+}
+
