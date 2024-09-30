95f80cc2f2863aea16ec77d99b3c4b8b7bfc3e51
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2024-09-30 18:08:33 -0300

    att modulos

--- Projeto/main.c
+++ Projeto/main.c
@@ -18,50 +18,22 @@ int main(void){
 
 
 
-void tela_sobre(void){
-    printf("\n");
-    printf("═══════════════════════════════════════════════════════════\n");
-    printf("═══   Universidade Federal Rio Grande do Norte        ═════\n");
-    printf("═══   Diciplina Programação  codigo:DCT1106           ═════\n");
-    printf("═══               Programa de Gestão                  ═════\n");
-    printf("═══   Para uma Loja de Material de Construção         ═════\n");
-    printf("═══════════════════════════════════════════════════════════\n");
-    printf("═══   O Projeto tem o objetivo Criar um programa      ═════\n");
-    printf("═══   de Gestão para uma Loja de Materais             ═════\n");
-    printf("═══   de Construção,tem como objetivo ajudar na       ═════\n");
-    printf("═══   administração da loja,sendo possivel administar ═════\n");
-    printf("═══   produtos,vendas,clientes,funcionarios e tambem  ═════\n");
-    printf("═══   relatorios                                      ═════\n");
-    printf("═══════════════════════════════════════════════════════════\n");
-
-}
-
-void tela_equipe(void){
-    printf("\n");
-    printf("═══════════════════════════════════════════════════════════\n");
-    printf("═══            Equipe de desenvolvimento              ═════\n");
-    printf("═══════════════════════════════════════════════════════════\n");
-    printf("═══            Leandro Sergio Da Silva                ═════\n");
-    printf("═══            Arthur De Medeiros Dantas              ═════\n");
-    printf("═══════════════════════════════════════════════════════════\n");
-}
-
-
-
 void tela_principal(void){
     int resp;
     printf("\n");
-    printf("═══════════════════════════════════════════════════════════\n");
-    printf("═══                SIG-Building                       ═════\n");
-    printf("═══════════════════════════════════════════════════════════\n");
-    printf("═══    Sistema de gestão de material de construção    ═════\n");
-    printf("═══════════════════════════════════════════════════════════\n");
-    printf("═══                [1] - Clientes                     ═════\n");
-    printf("═══                [2] - Funcionário                  ═════\n");
-    printf("═══                [3] - Sobre o Projeto              ═════\n");
-    printf("═══                [0] - Sair                         ═════\n");
+    printf("===========================================================\n");
+    printf("=====                SIG-Building                     =====\n");
+    printf("===========================================================\n");
+    printf("=====    Sistema de gestão de material de construção  =====\n");
+    printf("===========================================================\n");
+    printf("=====                [1] - Clientes                   =====\n");
+    printf("=====                [2] - Vendas                     =====\n");
+    printf("=====                [3] - Funcionário                =====\n");
+    printf("=====                [5] - Produto                    =====\n");
+    printf("=====                [4] - Sobre o Projeto            =====\n");
+    printf("=====                [0] - Sair                       =====\n");
     scanf("%i", &resp);
-    printf("═══════════════════════════════════════════════════════════\n");
+    printf("===========================================================\n");
     if (resp == 1 ){
         printf(" Modulo Clientes \n");
         //mod_client(); //função fazer compras
@@ -74,4 +46,32 @@ void tela_principal(void){
         printf("Resposta inválida. \n");
        
 }     
+}
+
+void tela_sobre(void){
+    printf("\n");
+    printf("===========================================================\n");
+    printf("=====   Universidade Federal Rio Grande do Norte      =====\n");
+    printf("=====   Diciplina Programação  codigo:DCT1106         =====\n");
+    printf("=====               Programa de Gestão                =====\n");
+    printf("=====   Para uma Loja de Material de Construção       =====\n");
+    printf("===========================================================\n");
+    printf("=====   O Projeto tem o objetivo Criar um programa    =====\n");
+    printf("=====   de Gestão para uma Loja de Materais           =====\n");
+    printf("=====   de Construção,tem como objetivo ajudar na     =====\n");
+    printf("=====   administração da loja,sendo possivel administa=====\n");
+    printf("=====   produtos,vendas,clientes,funcionarios e tambem=====\n");
+    printf("=====   relatorios                                    =====\n");
+    printf("===========================================================\n");
+
+}
+
+void tela_equipe(void){
+    printf("\n");
+    printf("===========================================================\n");
+    printf("=====            Equipe de desenvolvimento            =====\n");
+    printf("===========================================================\n");
+    printf("=====            Leandro Sergio Da Silva              =====\n");
+    printf("=====            Arthur De Medeiros Dantas            =====\n");
+    printf("===========================================================\n");
 }
