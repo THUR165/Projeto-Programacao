4ac13eb464b2fd64231d1c7ac2d718f4aef8cc62
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2024-10-01 21:12:32 -0300

    renomeando

--- Projeto/main.c
+++ Projeto/main.c
@@ -5,8 +5,8 @@
 
 //funções principais
 void tela_principal(void);
-//void tela_sobre(void);
-//void tela_equipe(void);
+void tela_sobre(void);
+void tela_equipe(void);
 
 
 //programa principal
@@ -46,4 +46,32 @@ void tela_principal(void){
         printf("Resposta inválida. \n");
        
 }     
+}
+
+void tela_sobre(void){
+    printf("\n");
+    printf("===========================================================\n");
+    printf("=====   Universidade Federal Rio Grande do Norte      =====\n");
+    printf("=====   Diciplina Programação  codigo:DCT1106         =====\n");
+    printf("=====               Programa de Gestão                =====\n");
+    printf("=====   Para uma Loja de Material de Construção       =====\n");
+    printf("===========================================================\n");
+    printf("=====   O Projeto tem o objetivo Criar um programa    =====\n");
+    printf("=====   de Gestão para uma Loja de Materais           =====\n");
+    printf("=====   de Construção,tem como objetivo ajudar na     =====\n");
+    printf("=====   administração da loja,sendo possivel administa=====\n");
+    printf("=====   produtos,vendas,clientes,funcionarios e tambem=====\n");
+    printf("=====   relatorios                                    =====\n");
+    printf("===========================================================\n");
+
+}
+
+void tela_equipe(void){
+    printf("\n");
+    printf("===========================================================\n");
+    printf("=====            Equipe de desenvolvimento            =====\n");
+    printf("===========================================================\n");
+    printf("=====            Leandro Sergio Da Silva              =====\n");
+    printf("=====            Arthur De Medeiros Dantas            =====\n");
+    printf("===========================================================\n");
 }
