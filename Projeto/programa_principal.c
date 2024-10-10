68f1a681280908b0d26ad944b4ce4b0a7b0166c1
arthur <103393571+THUR165@users.noreply.github.com>
2024-10-10 15:55:42 -0300

    apagado tela_client

--- Projeto/programa_principal.c
+++ Projeto/programa_principal.c
@@ -28,7 +28,6 @@ void main(void){
 
 
 //funções principais
-void tela_client(void);
 void tela_sobre(void);
 void tela_equipe(void);
 void modulo_funcionario(void);
@@ -48,7 +47,6 @@ void excluir_funcionario(void);
 
 //programa principal
 int main(void){
-    tela_client();
     tela_sobre();
     tela_equipe();
     modulo_funcionario();
@@ -86,28 +84,6 @@ int tela_main(void){
     return op;
 }
 
-void tela_client(void){
-    int resp;
-    printf("\n");
-    printf("===========================================================\n");
-    printf("===                SIG-Building                       =====\n");
-    printf("===========================================================\n");
-    printf("===    Sistema de gestão de material de construção    =====\n");
-    printf("===========================================================\n");
-    printf("===                [1] - Cadastrar Clientes           =====\n");
-    printf("===                [2] - Fazer Compras                =====\n");
-    scanf("%i", &resp);
-    printf("===========================================================\n");
-    if (resp == 1 ){
-        printf(" Modulo Clientes \n");
-        //cad_client(); //função fazer compras
-    } else if (resp == 2){
-        printf("Modulo Funcionário \n"); //function de cadastrar cliente
-    } else{
-        printf("Resposta inválida. \n");
-    }
-    
-}
 
 void tela_cadastrarc(void){
     int resp;
