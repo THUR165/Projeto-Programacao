644e04cbdd65c8a39b2b40fe4797f6c85fe87f5e
arthur <103393571+THUR165@users.noreply.github.com>
2024-10-10 15:19:26 -0300

    padronização das telas dos modulos

--- Projeto/programa_principal.c
+++ Projeto/programa_principal.c
@@ -195,9 +195,9 @@ void modulo_funcionario(void){
     printf("===========================================================\n");
     printf("======═            Modulo Funcionario                ======\n");
     printf("===========================================================\n");
-    printf("======═         1-produtos                           ======\n");
-    printf("======═         2-Funcionarios                       ======\n");
-    printf("======═         0-sair                               ======\n");
+    printf("======═         [1]-produtos                         ======\n");
+    printf("======═         [2]-Funcionarios                     ======\n");
+    printf("======═         [0]-sair                             ======\n");
     printf("===========================================================\n");
 }
 
@@ -206,11 +206,11 @@ void tela_produto(void){
     printf("===========================================================\n");
     printf("======═                Produto                       ======\n");
     printf("===========================================================\n");
-    printf("======═         1-cadastrar produto                  ======\n");
-    printf("======═         2-exibir produto                     ======\n");
-    printf("======═         3-modificar produto                  ======\n");
-    printf("======═         4-excluir produto                    ======\n");
-    printf("======═         0- sair                              ======\n");
+    printf("======═         [1]-cadastrar produto                ======\n");
+    printf("======═         [2]-exibir produto                   ======\n");
+    printf("======═         [3]-modificar produto                ======\n");
+    printf("======═         [4]-excluir produto                  ======\n");
+    printf("======═         [0]- sair                            ======\n");
     printf("===========================================================\n");
 }
 
@@ -264,11 +264,11 @@ void tela_funcionario(void){
     printf("===========================================================\n");
     printf("======═                Funcionario                   ======\n");
     printf("===========================================================\n");
-    printf("======═         1-cadastrar Funcionario              ======\n");
-    printf("======═         2-Exibir Funcionarios                ======\n");
-    printf("======═         3-modificar Funcionario              ======\n");
-    printf("======═         4-Excluir Funcionario                ======\n");
-    printf("======═         0-sair                               ======\n");
+    printf("======═         [1]-cadastrar Funcionario            ======\n");
+    printf("======═         [2]-Exibir Funcionarios              ======\n");
+    printf("======═         [3]-modificar Funcionario            ======\n");
+    printf("======═         [4]-Excluir Funcionario              ======\n");
+    printf("======═         [0]-sair                             ======\n");
     printf("===========================================================\n");
     printf("===========================================================\n");
     printf("===========================================================\n");
