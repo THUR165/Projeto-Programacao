204fed6b63f781ec5481512f4f16f42c22016f38
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2024-10-14 14:42:36 -0300

    Revert "removendo lixos"
    This reverts commit f4106f99e0ffa679b0ffee7980ebcf091dd71cbd.

---
+++ Projeto/src/vendas/mod_vendas.c
@@ -0,0 +1,30 @@
+//fazer compras(add ao carrinho)(mostrar lista de produtos cadastrados pelo modulo funcionário)
+//verificar compras/ tipo um extrato com valor.
+//altera compras(mostrar tipo o carrinho)
+// excluir produto da lista de compras(repor no estoque)
+#include <stdio.h>
+#include "./interface.h"
+int mod_vendas(void){
+    int op;
+    printf("Modulo de Vendas \n");
+    do{
+        op = mod_venda();
+        switch (op){
+            case 1: printf("Comprar produto");
+                break;
+            case 2: printf("Verificar compras");
+                break;
+            case 3: printf("Alterar compras");
+                break;
+            case 4: printf("Excluir compras");
+                break;
+            case 5: printf("Relátorio");
+        }
+    }while (op !=0);
+        tela_main();
+
+    return 0;
+    
+}
+   
+ 
