00e27b0897c1c9300fd2d4e185dc8ea45d724320
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2024-11-06 18:02:08 -0300

    edit cadastro

--- Projeto/src/clientes/cadastrarc.c
+++ Projeto/src/clientes/cadastrarc.c
@@ -2,15 +2,40 @@
 #include <stdlib.h>
 #include "cadastrarc.h"
 
+/*
+1 - Clientes:
+Nome:
+CPF:
+Data de Nascimento:
+Gênero:
+Telefone:
+Rua:
+*/
+
 void cadastrar_cliente(void){
-    char nome[50];
+    char nome; //espaço da variavel:indeterminado, alocação dinamica
     char cpf[20];
+    char nasc[11];
+    char gen[2];
+    char tel[16];
+    char rua;  //espaço da variavel:indeterminado, alocação dinamica
+    char cadastro;
     printf("\n===========================================================\n");
     printf("======═             Cadastrar Cliente                ======\n");
     printf("===========================================================\n");
-    printf("Digite o nome do cliente: ");
+    printf( "\n");
+    printf("Digite o nome do cliente: \n"); //espaço da variavel:indeterminado
     scanf(" %[^\n]", nome);
-    printf("Digite o CPF do cliente: ");
+    printf("Digite o CPF do cliente(123.456.789-12) \n");//espaço da variavel:16
     scanf("%s", cpf);
-    printf("===========================================================\n");
+    print("Data de Nascimento(xx/xx/xxxx): ->   \n"); //espaço da variavel:11
+    scanf("%s", nasc);
+    printf("Gênero(M/F): ->  \n"); //espaço da variavel:2
+    scanf("%s",gen);
+    printf("Digite Telefone:(xx) x xxxx-xxxx): ->  \n"); //espaço da variavel:16
+    scanf("%s", tel);
+    printf("Digite o nome da Rua: ->  \n");
+    scanf("%s", rua);
+
+    ~//cadastro = [nome,cpf,nasc,gen,tel,rua];
 }
